774f1d053bc137f3b8d69323133a3dcf980dc826
Abhay Balakrishnan <abhaybalakrishnan884@gmail.com>
2026-02-22 22:21:42 +0530

    update wm

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,14 @@
 export function Footer() {
   return (
     <footer className="flex h-21 bg-zinc-100 border-t-2 border-zinc-200 select-none justify-center text items-center font-clashgrotesk text-center max-sm:h-16">
-      <p className="font-medium text-4xl text-zinc-400/40 max-sm:text-3xl">
-        {"CSBS '27"}
-      </p>
+      <a
+        href="https://github.com/ABHAY-100"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-medium text-4xl text-zinc-400/40 max-sm:text-3xl transition-all duration-500 ease-out hover:scale-105 cursor-pointer inline-block"
+      >
+        {"ABHAY-100"}
+      </a>
     </footer>
   );
 }
